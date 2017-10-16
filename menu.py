d490c13712d4b3fc82287449039f26bed98f37e5
Preston Trieu <Preston@dhcp-v120-002.mobile.uci.edu>
2017-10-16 13:28:34 -0700

    Fixed menu error

--- menu.py
+++ menu.py
@@ -4,8 +4,9 @@ def menuRun():
 	print("""Select a choice from the options below:
 	A) Add a sibling.
 	B) Add a parent.
-	C) Update current member.
-	D) Exit application.""")
+	C) Cancel member selction.
+	D) Delete selected member.
+	E) Exit menu. \n""")
 	userChoice = input("Enter here: ")
 	while True:
 		if userChoice.rstrip().upper() == "A":
@@ -13,19 +14,21 @@ def menuRun():
 		elif  userChoice.rstrip().upper() == "B":
 			print("User has selected B. Adding parent. \n")
 		elif userChoice.rstrip().upper() == "C":
-			print("Deleting member... \n")
+			print("Canceling selection... \n")
 		elif userChoice.rstrip().upper() == "D":
+			print("Deleting member...\n")
+		elif userChoice.rstrip().upper() == "E":
 			print("Exiting Menu... \n")
 			break
 		else:
-			print("Invalid selection. Please make a choice from one of the menu options.\n")
+			print("Invalid selection. Please make a choice from one of the menu options.")
 		print("""Select a choice from the options below:
 		  A) Add a sibling.
 		  B) Add a parent.
 		  C) Delete member.
 		  D) Cancel member selection
 		  E) Exit application. \n""")
-		userChoice = input("Enter here:  ")
+		userChoice = input("Enter here:  \n")
 
 
 if __name__ == "__main__":
